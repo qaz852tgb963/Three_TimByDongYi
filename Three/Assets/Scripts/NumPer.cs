3b7befcdef2d26e678fef0fc00f0d6d91b52a934
qaz852tgb963 <65478824+qaz852tgb963@users.noreply.github.com>
2021-03-11 21:27:39 +0800

    110/03/11-END

---
+++ Three/Assets/Scripts/NumPer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class NumPer : MonoBehaviour
+{
+
+    #region 屬性
+
+    [Header("是否碰到牆壁_上下左右")]
+    public bool bHitWallUp;
+    public bool bHitWallDown;
+    public bool bHitWallLeft;
+    public bool bHitWallRight;
+    
+    [Header("是否碰到其他數字格子_上下左右")]
+    public bool bHitNumPerUp;
+    public bool bHitNumPerDown;
+    public bool bHitNumPerLeft;
+    public bool bHitNumPerRight;
+
+    #endregion
+
+
+    #region 函式
+
+
+
+    #endregion
+
+    
+    #region 事件
+
+    private void Start()
+    {
+        
+    }
+    
+    #endregion
+
+}
