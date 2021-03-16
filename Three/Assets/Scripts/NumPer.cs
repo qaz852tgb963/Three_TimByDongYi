191e76ce35a7b9f5292516c0e25c12655600fde1
qaz852tgb963 <65478824+qaz852tgb963@users.noreply.github.com>
2021-03-16 19:32:00 +0800

    FIN

--- Three/Assets/Scripts/NumPer.cs
+++ Three/Assets/Scripts/NumPer.cs
@@ -10,30 +10,149 @@ public class NumPer : MonoBehaviour
     public bool bHitWallDown;
     public bool bHitWallLeft;
     public bool bHitWallRight;
-    
+
     [Header("是否碰到其他數字格子_上下左右")]
     public bool bHitNumPerUp;
     public bool bHitNumPerDown;
     public bool bHitNumPerLeft;
     public bool bHitNumPerRight;
 
-    #endregion
-
-
-    #region 函式
-
-
+    [Header("射線長度")]
+    public float fHitLine = 90f;
+    //自己
+    private RectTransform rect;
 
     #endregion
 
+
+    #region 方法
+
+    //private void SettingLen()
+    //{
+    //    Gizmos.color = Color.black;
+    //    Gizmos.DrawRay(transform.position, Vector3.up * fHitLine);
+    //    Gizmos.color = Color.blue;
+    //    Gizmos.DrawRay(transform.position, Vector3.down * fHitLine);
+
+    //    Gizmos.color = Color.yellow;
+    //    Gizmos.DrawRay(transform.position, Vector3.left * fHitLine);
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawRay(transform.position, Vector3.right * fHitLine);
+    //}
+
+    public void CheckWall()
+    {
+        RaycastHit2D HitWallUp = Physics2D.Raycast(transform.position, Vector2.up, fHitLine, 1 << 11);
+        RaycastHit2D HitWallDown= Physics2D.Raycast(transform.position, Vector2.down, fHitLine, 1 << 11);
+        RaycastHit2D HitWallLeft= Physics2D.Raycast(transform.position, Vector2.left, fHitLine, 1 << 11);
+        RaycastHit2D HitWallRight= Physics2D.Raycast(transform.position, Vector2.right, fHitLine, 1 << 11);
+        
+        if (HitWallUp && HitWallUp.transform.name == "牆壁_上")
+        {
+            bHitWallUp = true;
+        }
+        else
+        {
+            bHitWallUp = false;
+        }
+        
+        if (HitWallDown && HitWallDown.transform.name == "牆壁_下")
+        {
+            bHitWallDown = true;
+        }
+        else
+        {
+            bHitWallDown = false;
+        }
+        
+        if (HitWallLeft && HitWallLeft.transform.name == "牆壁_左")
+        {
+            bHitWallLeft = true;
+        }
+        else
+        {
+            bHitWallLeft = false;
+        }
+        
+        if (HitWallRight && HitWallRight.transform.name == "牆壁_右")
+        {
+            bHitWallRight = true;
+        }
+        else
+        {
+            bHitWallRight = false;
+        }
+
+
+    }
     
+    public void CheckNumPer()
+    {
+        RaycastHit2D HitNumPerUp = Physics2D.Raycast(transform.position, Vector2.up, fHitLine, 1 << 12);
+        RaycastHit2D HitNumPerDown= Physics2D.Raycast(transform.position, Vector2.down, fHitLine, 1 << 12);
+        RaycastHit2D HitNumPerLeft= Physics2D.Raycast(transform.position, Vector2.left, fHitLine, 1 << 12);
+        RaycastHit2D HitNumPerRight= Physics2D.Raycast(transform.position, Vector2.right, fHitLine, 1 << 12);
+        
+        if (HitNumPerUp && HitNumPerUp.transform.tag == "NumPer_Tag")
+        {
+            bHitNumPerUp = true;
+        }
+        else
+        {
+            bHitNumPerUp = false;
+        }
+        
+        if (HitNumPerDown && HitNumPerDown.transform.tag == "NumPer_Tag")
+        {
+            bHitNumPerDown = true;
+        }
+        else
+        {
+            bHitNumPerDown = false;
+        }
+        
+        if (HitNumPerLeft && HitNumPerLeft.transform.tag == "NumPer_Tag")
+        {
+            bHitNumPerLeft = true;
+        }
+        else
+        {
+            bHitNumPerLeft = false;
+        }
+        
+        if (HitNumPerRight && HitNumPerRight.transform.tag == "NumPer_Tag")
+        {
+            bHitNumPerRight = true;
+        }
+        else
+        {
+            bHitNumPerRight = false;
+        }
+
+
+    }
+
+    #endregion
+
+
     #region 事件
 
     private void Start()
     {
-        
+        rect = GetComponent<RectTransform>();
     }
-    
+
+    private void Update()
+    {
+        CheckWall();
+        CheckNumPer();
+    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    settingLen();
+    //}
+
     #endregion
 
 }
---
+++ Three/Assets/Scripts/NumPerMsnager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class NumPerMsnager : MonoBehaviour
+{    
+    #region 屬性
+
+    [Header("生成的數字紙")]
+    public RectTransform NumPer_Instant;
+    [Header("生成的數字紙位置")]
+    public Vector2[] V2NumPer_L = {
+        new Vector2(-224,24),
+        new Vector2(-74,24),
+        new Vector2(76,24),
+        new Vector2(226,24),
+        
+        new Vector2(-224,-126),
+        new Vector2(-74,-126),
+        new Vector2(76,-126),
+        new Vector2(226,-126),
+
+        new Vector2(-224,-276),
+        new Vector2(-74,-276),
+        new Vector2(76,-276),
+        new Vector2(226,-276),
+        
+        new Vector2(-224,-426),
+        new Vector2(-74,-426),
+        new Vector2(76,-426),
+        new Vector2(226,-426),
+    };
+
+    #endregion
+}
